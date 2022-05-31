c47b415420bacd2ae9d33a663b52a732f298d554
weigang <s9460404@gmail.com>
2022-06-01 00:15:15 +0800

    build my web site on github

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
 <?php
 header('content-type:image/jpg;');
-$content=file_get_contents('https://tvr02.dimension.com.tw/OA/DCT_SHA.nsf/D3E48E98CC60150E482588240022770C/$FILE/0223r1.1.jpg');
+$content=file_get_contents('test.jpg');
 echo $content;
 ?>
 </body>
